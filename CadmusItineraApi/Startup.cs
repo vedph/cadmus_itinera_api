eca2c7601f594b3b4719511305d42327bb42176f
Myrmex <dfusi@hotmail.com>
2020-11-01 09:14:22 +0100

    Added forwarded header middleware

--- CadmusItineraApi/Startup.cs
+++ CadmusItineraApi/Startup.cs
@@ -30,6 +30,7 @@ using Cadmus.Api.Services.Messaging;
 using Cadmus.Api.Services;
 using CadmusItineraApi.Services;
 using System.Linq;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace CadmusItineraApi
 {
@@ -277,6 +278,13 @@ namespace CadmusItineraApi
         /// <param name="env">The environment.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-2.2#configure-a-reverse-proxy-server
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                    | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
